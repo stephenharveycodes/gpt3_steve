a41cfcd601cf7f8958ac14edde6c450784e75c56
Stephen Harvey <itsteveharvey@gmail.com>
2023-06-27 21:09:13 +1200

    Added css styles to WhatGPT3 component

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import "./whatGPT3.css";
 import { Feature } from "../../components";
+import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
   return (
-    <div className="gpt3__whatgp3 section__margin" id="wgpt3">
-      <div className="gpt3__whatgp3-feature">
+    <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
+      <div className="gpt3__whatgpt3-feature">
         <Feature />
       </div>
-      <div className="gpt3__whatgp3-heading">
-        <h1 className="gradient-text">
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
           The possibilities are beyond your imagination
         </h1>
         <p>Explore The Library</p>
       </div>
-      <div className="gpt3__whatgp3-container">
+      <div className="gpt3__whatgpt3-container">
         <Feature />
         <Feature />
         <Feature />
